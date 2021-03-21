0dc285509d269f8910a01ee0e52ef6382fd4651f
M_Abubakar <17-cs-40@students.uettaxila.edu.pk>
2021-03-21 19:44:01 +0500

    Islamabad whether app depoyment

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from app.views import index
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
